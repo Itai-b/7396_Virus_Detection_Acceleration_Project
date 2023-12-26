e7d2d6a86d59534ede35072a24fa767efa3b25d5
idanbaru <idan-b@campus.technion.ac.il>
2023-12-26 20:40:38 +0200

    Add Part B - Cuckoo Hash
    * Add libcuckoo subdir

---
+++ Part B - Cuckoo Hash/out/build/x64-debug/libcuckoo/libcuckoo-c/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/libcuckoo/libcuckoo-c
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/install/x64-debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/install/x64-debug/include/libcuckoo-c/cuckoo_table_template.h;F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/install/x64-debug/include/libcuckoo-c/cuckoo_table_template.cc")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  file(INSTALL DESTINATION "F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/install/x64-debug/include/libcuckoo-c" TYPE FILE FILES
+    "F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/libcuckoo/libcuckoo-c/cuckoo_table_template.h"
+    "F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/libcuckoo/libcuckoo-c/cuckoo_table_template.cc"
+    )
+endif()
+
---
+++ Part B - Cuckoo Hash/test.cpp
@@ -0,0 +1,12 @@
+﻿// Part B - Cuckoo Hash.cpp : Defines the entry point for the application.
+//
+
+#include "test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
