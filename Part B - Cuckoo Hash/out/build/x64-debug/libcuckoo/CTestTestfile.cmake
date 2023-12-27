abdd6db766b7a776983a6863c5d0363997b3e5dc
Itai.b <itai1497@gmail.com>
2023-12-27 21:57:11 +0200

    update Part B: * change usage to kuku libarary by microsoft.

--- Part B - Cuckoo Hash/out/build/x64-debug/libcuckoo/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/libcuckoo
-# Build directory: F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/build/x64-debug/libcuckoo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libcuckoo")
-subdirs("libcuckoo-c")
-subdirs("tests")
--- Part B - Cuckoo Hash/out/build/x64-debug/libcuckoo/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/libcuckoo/tests
-# Build directory: F:/Technion/Project_b/code/7396_Virus_Detection_Acceleration_Project/Part B - Cuckoo Hash/out/build/x64-debug/libcuckoo/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
