b1422495e8b3d609e2a49e2b38447d6b69919cb8
idanbaru <idan-b@campus.technion.ac.il>
2024-01-06 17:19:54 +0200

    Part B - fixed installation of libcuckoo
    * Remove microsoft/Kuku library
    * Add efficient/libcuckoo library
    * Install libcuckoo library
    * Built libcuckoo library with a simple example
    * Add Part B Guide for installation instructions
    *** TO RUN FOLLOW GUIDE TO INSTALL LIBCUCKOO IN RELEVANT DIR ***

---
+++ Part B - libcuckoo version/cuckoohash/src/cuckoohash.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include <libcuckoo/cuckoohash_map.hh>
+
+int main() {
+    libcuckoo::cuckoohash_map<int, std::string> table;
+
+    for (int i = 0; i < 100; i++) {
+        table.insert(i, "hello");
+    }
+
+
+    for (int i = 0; i < 101; i++) {
+        std::string out;
+
+        if (table.find(i, out)) {
+            std::cout << i << "  " << out << std::endl;
+        }
+        else {
+            std::cout << i << "  not found" << std::endl;
+        }
+    }
+}
